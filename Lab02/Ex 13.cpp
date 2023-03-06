9be2ca4db11d888f348d5b580e5eb9e98d79e9ec
enzoDamatoDev <enzodamato@outlook.com>
2023-03-06 00:16:29 -0300

    arruamr ex 13

--- Lab02/Ex 13.cpp
+++ Lab02/Ex 13.cpp
@@ -3,22 +3,21 @@
 using namespace std;
 
 int main() {
-	char str[100],novo;
-	int posicao;
+	char str[100];
+	int posicao,i;
 	cout<<"escreva sua mensagem:"<<endl;
 	cin >> str;
 	
-	cout<<"qual posicao deseja substituir?"<<endl;
+	cout<<"qual posicao deseja remover?"<<endl;
 	cin >> posicao;
 	while(posicao <0 || posicao > 99){
 		cout<<"minimo 0 maximo 99"<<endl;
 		cin >> posicao;
 	}
 	
-	cout<<"substituir por"<<endl;
-	cin >> novo;
-	
-	str[posicao] = novo;
+	for(i=posicao;i<100;i++){
+		str[i] = str[i+1];
+	}
 	
 	cout<<str<<endl;
     return 0;
