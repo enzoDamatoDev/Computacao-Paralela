eea18459c6b1a0a2d4fa721213355d65c37c6da5
enzoDamatoDev <enzodamato@outlook.com>
2023-03-12 14:45:20 -0300

    add comments

--- Lab02/fatorial.c
+++ Lab02/fatorial.c
@@ -3,23 +3,23 @@
 
 int main()
 {
-   int num, pip[2];
-   pipe(pip);
+   int num, pip[2]; 
+   pipe(pip); //cria o pipe
    printf ("Enter the number: ");
    scanf ("%d", &num);
-   int id = fork();
+   int id = fork(); // divide os processos
    int i;
-	if(id == 0){
+	if(id == 0){ // processo filho cuida dos numeros pares
 		int j = num;
 		close(pip[0]);
 		
 		for (i=1; j > 1; j = j -2) {
 		     i=i*j;
 		}
-		write(pip[1], &i, sizeof(j));
+		write(pip[1], &i, sizeof(j)); // escreve resposta no pipe
 		close(pip[1]);
 		
-	}else{
+	}else{ // processo pai cuida dos numeros impares
 		int j = num-1;
 		for (i=1; j > 1; j = j -2) {
 			 i=i*j;
@@ -27,9 +27,9 @@ int main()
 		int impar;
 		
 		close(pip[1]);
-		read(pip[0],&impar,sizeof(impar));
+		read(pip[0],&impar,sizeof(impar)); //le a resposta do processo filho
 		close(pip[0]);
-		int final = impar*i;
+		int final = impar*i; // multiplica as duas respostas
 		printf("The factorial of %d is %d\n",num,final);
 	}
 	return 0;
