1126451955dd5f94d431de06527c41f0ce075fe0
Ubuntu <ubuntu@ip-172-31-13-110.ec2.internal>
2023-03-30 14:38:04 +0000

    adiconar esqueleto

--- Lab05/pthreads.c
+++ Lab05/pthreads.c
@@ -9,7 +9,20 @@ int **B;
 int **C;
 
 void *calcula(void* id){
-	printf("oi da thread %ld de %d\n",(long) id, numThreads);
+	int sum,*row = (int*)id,col,k;
+	//for(row=1; row<tamanho; row +=2) //multiplica as linhas impares
+	//{
+	        for(col=0; col<tamanho; col++)
+	        {
+	            sum = 0;
+	            for(k=0; k<tamanho; k++)
+	            {
+	                sum += A[*row][k] * B[k][col];
+	            }
+		  
+	            C[*row][col] = sum;
+	        }
+	//}
 	return NULL;
 }
 
@@ -17,23 +30,39 @@ int main(int argc, char **argv){
 	long thread;
 	pthread_t *threadh;
 	tamanho = atoi(argv[1]);
-	int i;
-	A = (int *)malloc(tamanho*sizeof(int *));
+	int i,j;
+	A = (int **)malloc(tamanho*sizeof(int *));
 	for(i=0;i<tamanho;i++){
-		A[i] = (int)malloc(tamanho*sizeof(int));
+		A[i] = (int *)malloc(tamanho*sizeof(int));
 	}
 
-	threadh = malloc (numThreads*sizeof(pthread_t));
+	B = (int **)malloc(tamanho*sizeof(int *));
+	for(i=0;i<tamanho;i++){
+		B[i] = (int *)malloc(tamanho*sizeof(int));
+	}
+
+	C = (int **)malloc(tamanho*sizeof(int *));
+	for(i=0;i<tamanho;i++){
+		C[i] = (int *)malloc(tamanho*sizeof(int));
+	}
+
+	threadh = malloc (tamanho*sizeof(pthread_t));
 	
-	for(thread =0; thread<numThreads;thread++){
-		pthread_create(&threadh[thread],NULL,hello,(void *)thread);
+	for(thread =0; thread<tamanho;thread++){
+		pthread_create(&threadh[thread],NULL,calcula,(void *)thread);
 	}
 
 	printf("oi da main");
 
-	for(thread =0;thread<numThreads;thread++){
+	for(thread =0;thread<tamanho;thread++){
 		pthread_join(threadh[thread],NULL);
 	}
 	free(threadh);
+	for(i=0;i<tamanho;i++){
+		for(j-0;j<tamanho;j++){
+			printf("%d ",C[i][j]);
+		}
+		printf("\n");
+	}
 	return 0;
 }
