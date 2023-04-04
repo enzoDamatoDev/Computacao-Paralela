80d6142c9f2220588a7ffd8a67436d13f1e4eda0
Ubuntu <ubuntu@ip-172-31-13-110.ec2.internal>
2023-04-04 01:02:13 +0000

    pq o paralelo ta maior que o serial?

--- Lab05/pthreads.c
+++ Lab05/pthreads.c
@@ -26,14 +26,19 @@ void *calcula(void* id){
 	return NULL;
 }
 
-int main(int argc, char **argv){
-	if(argc<2){
-		printf("informe o tamanho da matriz!");
-		exit(1);
+void calculaSerial(int tamanho){
+	int i,j,k,sum;
+	for(i=0;i<tamanho;i++){
+		for(j=0;j<tamanho;j++){
+			sum=0;
+			for(k=0;k<tamanho;k++){
+				sum += A[i][k] * B[k][j];
+			}
+			C[i][j] = sum;
+		}
 	}
-	long thread;
-	pthread_t *threadh;
-	tamanho = atoi(argv[1]);
+}
+void inicializar(int tamanho){
 	int i,j,count=0;
 	A = (int **)malloc(tamanho*sizeof(int *));
 	for(i=0;i<tamanho;i++){
@@ -55,19 +60,36 @@ int main(int argc, char **argv){
 	for(i=0;i<tamanho;i++){
 		C[i] = (int *)malloc(tamanho*sizeof(int));
 	}
+}
 
+int main(int argc, char **argv){
+	if(argc<2){
+		printf("informe o tamanho da matriz!");
+		exit(1);
+	}
+	long thread;
+	pthread_t *threadh;
+	tamanho = atoi(argv[1]);
+	int i,j;	
+	inicializar(tamanho);
+	
 	threadh = malloc (tamanho*sizeof(pthread_t));
 	
 	for(thread =0; thread<tamanho;thread++){
 		pthread_create(&threadh[thread],NULL, calcula,(void *)thread);
 	}
-	//if(tamanho == 0 && )
-	printf("oi da main\n");
 
 	for(thread =0;thread<tamanho;thread++){
 		pthread_join(threadh[thread],NULL);
 	}
 	free(threadh);
+	
+	
+	if(tamanho == 0 && argc == 3){
+		tamanho = atoi(argv[2]);
+		inicializar(tamanho);
+		calculaSerial(tamanho);
+	}
 	for(i=0;i<tamanho;i++){
 		for(j=0;j<tamanho;j++){
 			printf("%d ",C[i][j]);
